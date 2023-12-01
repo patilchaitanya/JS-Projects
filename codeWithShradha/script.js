3480675aca0fc2186b253cd0fd97cd108c7b358a
Chaitanya Patil <chaitanya.191423104@vcet.edu.in>
2023-12-01 15:31:19 +0530

    1. Added IMP_topics for interview.txt 2. New topics String and Array Practice questions.

--- codeWithShradha/script.js
+++ codeWithShradha/script.js
@@ -1,3 +1,5 @@
+//Practice Qs1 [loops] : Guess the number game 
+
 let value = 25;
 
 let attempts = 4;
@@ -7,10 +9,70 @@ for (let i=0; i<=attempts; i++ ){
         alert("Number greater , jarasa chota socho!");
         attempts--;
     } else if (userValue < 25) {
-        alert("Jarasa bada socho");
+        alert("Number Smaller, jarasa bada socho!");
         attempts--;
     } else {
-        alert ("correct Number: ", userValue);
+        alert ("Congratulations! Guessed the correct number");
         break;
     }
 }
+if (attempts === 0) {
+    alert("Chances overr!!, Reload to restart the game.");
+}
+
+
+//Practice Qs2 [Strings]: prompt full username and give them a username
+
+let fullName = prompt("Enter your full name : ");
+let username = "@" + fullName + fullName.length;
+console.log(username); 
+
+
+//Practice Qs3 [Arrays]: Find Avg of entire class given an array
+
+let marks = [12, 13, 14, 15, 16, 17];
+let sum = 0;
+for ( let val of marks){
+    sum += val;
+}
+console.log(sum);
+let avg = sum / marks.length;
+console.log(`the avg marks of class are ${avg}`);
+
+
+//Prac Qs4 [Arrays]: Applying final 10% off on an array of price stored.
+
+let items = [250, 645, 300, 900, 50];
+let offer = 10;
+
+let i = 0; 
+for (let val of items){
+    console.log(`Value : ${val}`);
+    let discount = val * offer /100;
+    let finalPrice = val - discount;
+    items[i] = finalPrice;
+    console.log(`Value after offer : ${items[i]} `);
+    i++;
+}
+
+
+// OR
+
+for (let i = 0;  i < items.length-1; i++) {
+    let discount = items[i] * offer / 100;
+    items[i] = items[i] - discount;
+}
+
+
+//Prac Qs5 [Array methods] remove,add in array
+
+let companies = [ "Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+companies.shift()
+console.log(companies);
+
+companies.splice(1,1, "OLa");
+console.log(companies);
+
+companies.push("Amazon");
+console.log(companies);
